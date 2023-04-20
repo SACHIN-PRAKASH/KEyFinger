20d16a9f9afeaa0e8761f23de562e60489128b50
Sachin Prakash <sachin.prakash553@gmail.com>
2023-04-20 23:52:07 +0000

    21 April

---
+++ dist/index.cb6f86c2.js
@@ -0,0 +1,2 @@
+const e=20,n="Hey Buddy! it's KeyFinger\nPlease enter your information :)\n\n";let t=0;!function o(){t<n.length&&(document.querySelector(".command-line").textContent+=n.charAt(t),t++,setTimeout(o,e))}();
+//# sourceMappingURL=index.cb6f86c2.js.map
